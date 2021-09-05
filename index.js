3a1c2530b7a1121c5f6f893326094a1de80cc78e
Adams Mohammed <moadams847@gmail.com>
2021-09-05 20:18:20 +0000

    add firebase

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 // ---
 const table = document.querySelector(".dataAdded");
 const form = document.querySelector("form");
-
 // --
-const addQuestions = (data, id) => {
+const addQuestions = (data, id, index) => {
   let time = data.created_at.toDate();
 
   let html = `
-<tr>
+<tr class="records" data-id="${id}" >
     <th scope="row">${id}</th>
     <td>${data.questions}</td>
     <td>${data.answers}</td>
     <td>${time}</td>
+    <td class=""><i class="far fa-lg fa-edit edit text-dark"></i></td>
+    <td class=""><i class="fas fa-lg fa-trash delete text-danger"></i></td>
     </tr>
 `;
   table.innerHTML += html;
@@ -21,23 +22,32 @@ const addQuestions = (data, id) => {
 // real time listener
 
 // delete in real time
+const deleteQuestions = (id) => {
+  const records = document.querySelectorAll(".records");
+  records.forEach((record) => {
+    if (record.getAttribute("data-id") === id) {
+      record.remove();
+    }
+  });
+};
 
-// add in real time
+// retrieve data in real time
 const unsub = db.collection("Q&A").onSnapshot((snapshot) => {
-  snapshot.docChanges().forEach((change) => {
+  snapshot.docChanges().forEach((change, index) => {
     const doc = change.doc;
-    console.log(doc.data());
+    // console.log(doc.data());
     if (change.type === "added") {
-      addQuestions(doc.data(), doc.id);
+      addQuestions(doc.data(), doc.id, index);
+    } else if (change.type === "removed") {
+      deleteQuestions(doc.id);
     }
   });
 });
 
 // ---
-// write to DB / create/add 2
+// add data to firestore
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-
   const now = new Date();
   const qna = {
     questions: form.question.value,
@@ -53,4 +63,19 @@ form.addEventListener("submit", (e) => {
       console.log(err);
     });
   form.reset();
+});
+
+// delete data from firestore on button click
+table.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    console.log("clicked");
+    const id = e.target.parentElement.parentElement.getAttribute("data-id");
+    console.log(id);
+    db.collection("Q&A")
+      .doc(id)
+      .delete()
+      .then(() => {
+        console.log("Q&A delete");
+      });
+  }
 });
