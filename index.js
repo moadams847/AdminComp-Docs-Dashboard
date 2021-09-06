a7c44eb7ea2ee95790aaf5251b307a4333f377e4
Adams Mohammed <moadams847@gmail.com>
2021-09-06 00:27:50 +0000

    edit in real time

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 // ---
 const table = document.querySelector(".dataAdded");
 const form = document.querySelector("form");
+const editForm = document.querySelector(".editForm");
+
+// ==========================================================================
 // --
+// upadate ui in real time when data is edited
 const addQuestions = (data, id, index) => {
   let time = data.created_at.toDate();
 
@@ -11,17 +15,24 @@ const addQuestions = (data, id, index) => {
     <td>${data.questions}</td>
     <td>${data.answers}</td>
     <td>${time}</td>
-    <td class=""><i class="far fa-lg fa-edit edit text-dark"></i></td>
-    <td class=""><i class="fas fa-lg fa-trash delete text-danger"></i></td>
+    <td class="">
+    <i
+      data-bs-toggle="modal"
+      data-bs-target="#exampleModal"
+      data-bs-whatever="@mdo"
+      class="far edit fa-lg fa-edit"
+    ></i>
+  </td>    <td class=""><i class="fas fa-lg fa-trash delete text-danger"></i></td>
     </tr>
 `;
   table.innerHTML += html;
 };
 
+// ===========================================================================
 // ---
 // real time listener
 
-// delete in real time
+// delete data from ui in real time
 const deleteQuestions = (id) => {
   const records = document.querySelectorAll(".records");
   records.forEach((record) => {
@@ -32,10 +43,10 @@ const deleteQuestions = (id) => {
 };
 
 // retrieve data in real time
-const unsub = db.collection("Q&A").onSnapshot((snapshot) => {
+db.collection("Q&A").onSnapshot((snapshot) => {
   snapshot.docChanges().forEach((change, index) => {
     const doc = change.doc;
-    // console.log(doc.data());
+    // console.log(change);
     if (change.type === "added") {
       addQuestions(doc.data(), doc.id, index);
     } else if (change.type === "removed") {
@@ -43,7 +54,7 @@ const unsub = db.collection("Q&A").onSnapshot((snapshot) => {
     }
   });
 });
-
+// =====================================================================
 // ---
 // add data to firestore
 form.addEventListener("submit", (e) => {
@@ -65,8 +76,13 @@ form.addEventListener("submit", (e) => {
   form.reset();
 });
 
+// =====================================================================
+// ---
 // delete data from firestore on button click
+// or get values of edit
+
 table.addEventListener("click", (e) => {
+  // delete from firestore
   if (e.target.classList.contains("delete")) {
     console.log("clicked");
     const id = e.target.parentElement.parentElement.getAttribute("data-id");
@@ -77,5 +93,64 @@ table.addEventListener("click", (e) => {
       .then(() => {
         console.log("Q&A delete");
       });
+
+    // get form values
+  } else if (e.target.classList.contains("edit")) {
+    const id = e.target.parentElement.parentElement.getAttribute("data-id");
+    const question =
+      e.target.parentElement.parentElement.children[1].textContent;
+    const answer = e.target.parentElement.parentElement.children[2].textContent;
+    editForm.question.value = question;
+    editForm.answer.value = answer;
+    editForm.id.value = id;
   }
 });
+
+// ==========================================================================
+//---
+// edit ui
+const editQuestion = (data, id) => {
+  const records = document.querySelectorAll(".records");
+  records.forEach((record) => {
+    if (record.getAttribute("data-id") === id) {
+      let time = data.created_at.toDate();
+      record.children[1].textContent = data.questions;
+      record.children[2].textContent = data.answers;
+      record.children[3].textContent = time;
+    }
+  });
+};
+
+// ---
+// update
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const id = editForm.id.value;
+  const now = new Date();
+  const qna = {
+    questions: editForm.question.value,
+    answers: editForm.answer.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now),
+  };
+
+  // add update to db
+  db.collection("Q&A")
+    .doc(id)
+    .update(qna)
+    .then(() => {
+      console.log("question edited");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  // query db by id
+  db.collection("Q&A")
+    .doc(id)
+    .get()
+    .then((snapshot) => {
+      // console.log(snapshot.data());
+      editQuestion(snapshot.data(), id);
+    });
+});
+// ======================================================================
