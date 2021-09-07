3f240f795adfcd03d02a8d40f2578549664573bf
Adams Mohammed <moadams847@gmail.com>
2021-09-07 18:16:13 +0000

    work on about

---
+++ about.js
@@ -0,0 +1,90 @@
+// ---
+const cardContent = document.querySelector(".cardContent");
+const aboutForm = document.querySelector(".aboutForm");
+
+// ---
+const updateUi = (data, id, index) => {
+  let time = data.created_at.toDate();
+
+  let html = `  
+  <div data-id="${id}" class="holdCardBtn">
+  <div class="card bg-light">
+  <div class="card-body">
+    <p class="card-text lead mb-3">
+     ${data.about}
+    </p>
+  </div>
+</div>
+<!--  -->
+<div class="mt-3">
+<button class="btn btn-primary edit">Edit</button>
+</div>
+</div>
+`;
+  cardContent.innerHTML += html;
+};
+
+// ---
+const editAbout = (data, id) => {
+  let aboutIntUI = document.querySelector(".holdCardBtn");
+  if (aboutIntUI.getAttribute("data-id") === id) {
+    const cardText = document.querySelector(".card-text");
+    cardText.textContent = data.about;
+  }
+};
+
+// ---
+// retrieve data in real time
+db.collection("About")
+  .orderBy("created_at", "desc")
+  .onSnapshot((snapshot) => {
+    snapshot.docChanges().forEach((change, index) => {
+      const doc = change.doc;
+      // console.log(change);
+      if (change.type === "added") {
+        updateUi(doc.data(), doc.id, index);
+        console.log(doc.data(), doc.id);
+      } else if (change.type === "modified") {
+        editAbout(doc.data(), doc.id);
+      }
+    });
+  });
+
+// ---
+cardContent.addEventListener("click", (e) => {
+  if (e.target.classList.contains("edit")) {
+    const text =
+      e.target.parentElement.parentElement.firstElementChild.firstElementChild
+        .firstElementChild.textContent;
+
+    aboutForm.about.value = text.trim();
+  }
+});
+
+// ---
+aboutForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const aboutInListener = document.querySelector(".holdCardBtn");
+  const id = aboutInListener.getAttribute("data-id");
+  const now = new Date();
+
+  const about = {
+    about: aboutForm.about.value,
+    created_at: firebase.firestore.Timestamp.fromDate(now),
+  };
+
+  // add update to db
+  db.collection("About")
+    .doc(id)
+    .update(about)
+    .then(() => {
+      console.log("question edited");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  //
+  aboutForm.reset();
+});
--- files.js
+++ files.js
@@ -1,1 +1,9 @@
-alert("hi");
+// ---
+// console.log(storageRef);
+const textForm = document.querySelector(".textForm");
+const filesRef = storageRef.child("AminComp-Docs-Files/");
+console.log(filesRef);
+
+textForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
