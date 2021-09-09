c77149b8f8dd7d30a56a0c2a7bf276ca1f06334e
Adams Mohammed <moadams847@gmail.com>
2021-09-09 15:40:16 +0000

    delete files

--- files.js
+++
@@ -1,9 +0,0 @@
-// ---
-// console.log(storageRef);
-const textForm = document.querySelector(".textForm");
-const filesRef = storageRef.child("AminComp-Docs-Files/");
-console.log(filesRef);
-
-textForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
